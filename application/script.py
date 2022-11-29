48898ecef5c90fec80f55561bf5039e70fcd9ba4
razmo <omeiriramzy.hotmail.com>
2022-11-29 15:55:55 +0100

    a

--- application/script.py
+++ application/script.py
@@ -5,7 +5,7 @@ with open('socrate.json', 'r') as myfile:
  data=myfile.read()
 obj = json.loads(data)
 for i in range(10): 
-tmp = obj['audio_features'][random.randint(0,20)]['id']
+    tmp = obj['audio_features'][random.randint(0,20)]['id']
 print("mon id est : "+tmp)
 myfile.close()
 
