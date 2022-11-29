e3918429f6a76ccbf745b337f52e175210bcf4ed
razmo <omeiriramzy.hotmail.com>
2022-11-29 15:42:07 +0100

    script

--- application/script.py
+++ application/script.py
@@ -1,11 +1,11 @@
 import json 
 import random
-def recup_id(urljson, idx):
-    with open('socrate.json', 'r') as myfile:
-     data=myfile.read()
-    obj = json.loads(data)
-    for i in range(idx): 
-     tmp = obj['audio_features'][random.randint(0,20)]['id']
-     print("mon id est : "+tmp)
-    myfile.close()
+
+with open('socrate.json', 'r') as myfile:
+ data=myfile.read()
+obj = json.loads(data)
+for i in range(idx): 
+tmp = obj['audio_features'][random.randint(0,20)]['id']
+print("mon id est : "+tmp)
+myfile.close()
 
